6f0c12206424ea7c82ca2885ce149f6e8d69a596
Sonu Sindhu <sonusindhu10@yahoo.com>
2022-05-11 21:49:23 +0530

    created axios interceptor to inject user token

---
+++ src/config.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import AuthService from "./services/auth.service";
+const API_URL = process.env.REACT_APP_API_ENDPOINT;
+
+const instance = axios.create({
+  baseURL: API_URL,
+});
+
+instance.interceptors.request.use(
+  function (config) {
+    const user = AuthService.getCurrentUser();
+    if (user) {
+      config.headers.common["token"] = user.token;
+    }
+    config.headers.common["allowOrigins"] = "*";
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
