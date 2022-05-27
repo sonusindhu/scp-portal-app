729ad273b597a6b93a33b7cc77ee3b2ff353024e
Sonu Sindhu <sonusindhu10@yahoo.com>
2022-05-27 21:07:22 +0530

    minor updates/fixes

--- src/services/company.service.tsx
+++ src/services/company.service.tsx
@@ -2,7 +2,9 @@ import axios from "../utils/config.util";
 const API_URL = process.env.REACT_APP_API_ENDPOINT;
 
 const deleteCompanies = (ids: number[]) => {
-  return axios.post(`${API_URL}company/deleteRange`, { ids });
+  return axios
+    .post(`${API_URL}company/deleteRange`, { ids })
+    .then(({ data }) => data);
 };
 
 const find = (id: number) => {
