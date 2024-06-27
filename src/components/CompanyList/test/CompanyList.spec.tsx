1b7c08dbdb62f681361c07de84b2c483ea4de273
Sonu Sindhu <sonusindhu10@yahoo.com>
2024-06-27 09:40:26 +0530

    first test cases - Company List

---
+++ src/components/CompanyList/test/CompanyList.spec.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import CompanyList from "../CompanyList";
+import "@testing-library/jest-dom";
+import { BrowserRouter } from "react-router-dom";
+
+it("should render the component Company List", () => {
+  render(
+    <BrowserRouter>
+      <CompanyList />
+    </BrowserRouter>
+  );
+
+  const heading = screen.getByRole("heading");
+
+  expect(heading).toBeInTheDocument();
+});
