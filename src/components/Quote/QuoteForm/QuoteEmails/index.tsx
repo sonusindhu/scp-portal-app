832893d40ab1d81c205f6f54e31b126ef3d37146
Sonu Sindhu <sonusindhu10@yahoo.com>
2024-10-02 16:57:46 +0530

    setup email

--- src/components/Quote/QuoteForm/QuoteEmails/index.tsx
+++ src/components/Quote/QuoteForm/QuoteEmails/index.tsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import QuoteService from "../../../../services/quote.service";
 import EmailList from "../../../../shared/components/Emails/EmailList";
 import { Grid } from "@material-ui/core";
 import EmailForm from "../../../../shared/components/Emails/EmailForm";
 import { Email } from "../../../../shared/models/Email";
+import EmailService from "../../../../services/email.service";
 
 const QuoteEmails = () => {
   let { id } = useParams();
   let [emails, setEmails] = useState<Email[]>([]);
-  let [email, setEmail] = useState<Partial<Email>>({});
+  let [email, setEmail] = useState<Partial<Email>>({
+    type: "quote",
+    quoteId: id ? +id : undefined,
+  });
 
   const onSuccess = (event: Email) => {
     const email = [event];
@@ -17,22 +20,22 @@ const QuoteEmails = () => {
   };
 
   useEffect(() => {
-    if(id){
-      QuoteService.getEmails(+id).then((response: Email[]) => setEmails(response));
+    if (id) {
+      EmailService.get({ quoteId: id }).then((response) =>
+        setEmails(response.result)
+      );
     }
   }, []);
 
   return (
     <Grid container spacing={2}>
-      
-      <Grid item xs={4}>        
-        <EmailForm email={email} onSuccess={onSuccess} />
-      `</Grid>
+      <Grid item xs={4}>
+        <EmailForm email={email} onSuccess={onSuccess} />`
+      </Grid>
 
       <Grid item xs={8}>
         <EmailList emails={emails} />
       </Grid>
-
     </Grid>
   );
 };
