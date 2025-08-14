91e394d6054aaa8f888818dc3ac7db31dea3bdeb
sonu_taazaa <sonu@taazaa.com>
2025-08-14 20:38:05 +0530

    changes in header

--- web-app/src/services/auth-header.ts
+++ web-app/src/services/auth-header.ts
@@ -1,10 +1,10 @@
-import { AxiosRequestHeaders } from 'axios';
 export default function authHeader() {
   const userAuth = localStorage.getItem("user");
   const user = userAuth ? JSON.parse(userAuth) : null;
+
   if (user && user.token) {
-    return { token: user.token } as AxiosRequestHeaders;
+    return { token: user.token }; // Let TypeScript infer the type
   } else {
-    return {} as AxiosRequestHeaders;
+    return {};
   }
 }
